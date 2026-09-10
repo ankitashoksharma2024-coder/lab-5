9220a837591620ac4e9592cd547a531289169d77
ankitashoksharma2024-coder <ankit.ashoksharma2024@vitstudent.ac.in>
2026-09-10 10:49:38 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-def add(x,y):
-    return x+y
-def subtract(x,y):
-    return x-y
+def find_max(numbers):
+    return max(numbers)
+def count_evens(numbers):
+    return len([n for n in numbers if n%2==0])
