28a8543e76a2b7cf69cfd8b4fcb92ddb423c7164
ankitashoksharma2024-coder <ankit.ashoksharma2024@vitstudent.ac.in>
2026-09-10 11:20:06 +0530

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
-def find_max(numbers):
-    return max(numbers)
-def count_evens(numbers):
-    return len([n for n in numbers if n%2==0])
+print("Deploying applicaion version 1.0...")
+print("Deployment complete")
